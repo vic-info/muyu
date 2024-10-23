3ce8f02fbf3258bac0242bc79af50807cc5f0d46
Vic <vic.hsun.llc@gmail.com>
2024-10-23 12:39:10 -0700

    Initial Commit

---
+++ muyu/muyuApp.swift
@@ -0,0 +1,17 @@
+//
+//  muyuApp.swift
+//  muyu
+//
+//  Created by HLF_SUN on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct muyuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
